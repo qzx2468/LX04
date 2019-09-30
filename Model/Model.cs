0651b8e9b04c50c489bb607fb4e192ababa5a7b7
qzx2468 <529643918@qq.com>
2019-09-30 15:09:20 +0800

    Lx04

---
+++ Model/Model.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Model
+{
+    public class Activity
+    {
+        public int activityId { get; set; }
+        public string activityName { get; set; }
+        public DateTime endTime { get; set; }
+       
+    }
+    public class WorkInfo
+    { 
+        public int workId { get; set; }
+    }
+    public class Comment
+    {
+        public int commentId { get; set; }
+       
+    }
+    public class WorkFindPage : Page
+    {
+        public string workName { get;set }
+    }
+}
